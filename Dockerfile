c4982efd0af1b40c05008a84a85a0f70c2748e0c
Jakub Podeszwik <jakub.podeszwik@grupawp.pl>
2014-12-09 15:48:13 +0100

    elasticsearch-1.4.1

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+#
+# jpodeszwik/elasticsearch Dockerfile
+#
+FROM dockerfile/java:oracle-java8 
+
+# Install elasticsearch
+ENV ES_CONFIG_PATH /data/config/elasticsearch.yml 
+ENV ES_VERSION 1.4.1
+RUN \
+  cd /tmp && \
+  wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.tar.gz && \
+  tar xvzf elasticsearch-$ES_VERSION.tar.gz && \
+  rm -f elasticsearch-$ES_VERSION.tar.gz && \
+  mv /tmp/elasticsearch-$ES_VERSION /opt/elasticsearch-$ES_VERSION && \
+  ln -s /opt/elasticsearch-$ES_VERSION /opt/elasticsearch
+
+# Install plugins
+RUN \
+  cd /opt/elasticsearch/bin && \
+  ./plugin --install royrusso/elasticsearch-HQ && \
+  ./plugin --install mobz/elasticsearch-head && \
+  ./plugin --install karmi/elasticsearch-paramedic && \
+  ./plugin --install lukas-vlcek/bigdesk && \
+  ./plugin --install jettro/elasticsearch-gui && \
+  ./plugin --install lmenezes/elasticsearch-kopf
+
+# Basic configuration
+ENV ES_CLUSTER_NAME elasticsearch
+ENV ES_NODE_MASTER true
+ENV ES_NODE_DATA true  
+
+VOLUME ["/data"]
+
+WORKDIR /data
+
+ADD run.sh /bin/run.sh
+CMD /bin/run.sh
+
---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function set_es_config {
+    echo "$1: $2" >> $ES_CONFIG_PATH
+}
+
+if [ ! -f $ES_CONFIG_PATH ]; then
+    echo "Config file does not exist. Configuring elasticsearch."
+
+    mkdir -p /data/logs
+    mkdir -p /data/data
+    cp -r /opt/elasticsearch/config/ /data/
+    
+    set_es_config "path.conf" "/data/config"
+    set_es_config "path.data" "/data/data"
+    set_es_config "path.logs" "/data/logs"
+    set_es_config "script.disable_dynamic" "true" 
+    
+    set_es_config "cluster.name" $ES_CLUSTER_NAME
+
+    if [ -n "$ES_NODE_NAME" ]; then
+        set_es_config "node.name" $ES_NODE_NAME 
+    fi
+
+    set_es_config "node.master" $ES_NODE_MASTER
+    set_es_config "node.data" $ES_NODE_DATA
+
+    if [ -n "$ES_UNICAST_HOSTS" ]; then
+        set_es_config "discovery.zen.ping.multicast.enabled" "false"
+        set_es_config "discovery.zen.ping.unicast.hosts" $ES_UNICAST_HOSTS
+    fi
+
+    if [ -n "$ES_NETWORK_PUBLISH_HOST" ]; then
+        set_es_config "network.publish_host" $ES_NETWORK_PUBLISH_HOST
+    fi
+
+    if [ -n "$ES_TRANSPORT_TCP_PORT" ]; then
+        set_es_config "transport.tcp.port" $ES_TRANSPORT_TCP_PORT
+    fi
+fi
+
+exec /opt/elasticsearch/bin/elasticsearch -Des.config=$ES_CONFIG_PATH
+
